322d34db365d011f4ab15a4572e0903460256451
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-09-04 16:55:18 -0300

    cria tela de login

--- app/src/main/java/com/luciane/ccta/MainActivity.kt
+++ app/src/main/java/com/luciane/ccta/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.luciane.ccta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.luciane.ccta.model.User
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val loginText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val passwordText = findViewById<EditText>(R.id.editTextTextPassword)
+        val buttonLogin = findViewById<Button>(R.id.button)
+
+        buttonLogin.setOnClickListener {
+            val userInfo = Intent(this, HomeActivity::class.java)
+
+            val user = User(loginText.text.toString(), passwordText.text.toString())
+            userInfo.putExtra("User", user);
+            startActivity(userInfo)
+        }
     }
 }
