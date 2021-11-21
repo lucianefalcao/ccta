14a5bf5204cf11cfa8ada84f7b997cd31e083d6a
moura1001 <gmouraneto01@gmail.com>
2021-11-21 11:04:39 -0300

    commit com implementação do envio e recebimento de mensagens através da integração com o Firebase Database

--- app/src/main/java/com/luciane/ccta/activity/chat/ChatActivity.kt
+++ app/src/main/java/com/luciane/ccta/activity/chat/ChatActivity.kt
@@ -2,13 +2,24 @@ package com.luciane.ccta.activity.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import com.luciane.ccta.R
 import com.luciane.ccta.model.ChatMessage
 
 class ChatActivity : AppCompatActivity() {
+    companion object{
+        val TAG = "ChatScreen"
+        val FROM_ID = "testUser"
+        val TO_ID = "admin"
+    }
+
     var recyclerViewChatLog: RecyclerView? = null
     var chatAdapter = ChatAdapter()
 
@@ -29,37 +40,72 @@ class ChatActivity : AppCompatActivity() {
     }
 
     private fun listenForMessages(){
-        val chatMessage1 = ChatMessage("", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
-            "", "", System.currentTimeMillis()/1000,
-            ChatMessage.MessageType.RIGHT)
-        val chatMessage2 = ChatMessage("", "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
-            "", "", System.currentTimeMillis()/1000,
-            ChatMessage.MessageType.LEFT)
-        val chatMessage3 = ChatMessage("", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "", "", System.currentTimeMillis()/1000,
-            ChatMessage.MessageType.RIGHT)
-        val chatMessage4 = ChatMessage("", "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
-            "", "", System.currentTimeMillis()/1000,
-            ChatMessage.MessageType.LEFT)
+        val fromId = FROM_ID
+        val toId = TO_ID
 
-        chatAdapter.addMessage(chatMessage1)
-        chatAdapter.addMessage(chatMessage2)
-        chatAdapter.addMessage(chatMessage3)
-        chatAdapter.addMessage(chatMessage4)
+        val ref = FirebaseDatabase.getInstance().getReference("/user-messages/$fromId/$toId")
+        ref.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val chatMessage = snapshot.getValue(ChatMessage::class.java)
+                Log.d(TAG, chatMessage?.text!!)
+                if(chatMessage.fromId == FROM_ID){
+                    chatMessage.type = ChatMessage.MessageType.RIGHT
+                } else{
+                    chatMessage.type = ChatMessage.MessageType.LEFT
+                }
+                chatAdapter.addMessage(chatMessage)
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     private fun performSendMessage(){
-        val text = findViewById<EditText>(R.id.editTextUserInputChat).text.toString()
+        val editText = findViewById<EditText>(R.id.editTextUserInputChat)
+        val text = editText.text.toString()
 
-        val chatMessage = ChatMessage("", text, "",
-                                    "", System.currentTimeMillis()/1000)
+        val fromId = if(chatAdapter.itemCount % 2 == 0) FROM_ID else TO_ID
+        val toId = if(chatAdapter.itemCount % 2 == 0) TO_ID else FROM_ID
 
-        if(chatAdapter.itemCount % 2 == 0){
-            chatMessage.type = ChatMessage.MessageType.RIGHT
-        } else{
-            chatMessage.type = ChatMessage.MessageType.LEFT
-        }
+        val refFrom = FirebaseDatabase.getInstance()
+            .getReference("/user-messages/$fromId/$toId").push()
+        val refTo = FirebaseDatabase.getInstance()
+            .getReference("/user-messages/$toId/$fromId").push()
 
-        chatAdapter.addMessage(chatMessage)
+        val chatMessage = ChatMessage(refFrom.key!!, text, fromId,
+                                    toId, System.currentTimeMillis()/1000)
+
+        refFrom.setValue(chatMessage)
+            .addOnSuccessListener {
+                Log.d(TAG, "Saved our chat message: ${refFrom.key}")
+                editText.text.clear()
+                recyclerViewChatLog!!.scrollToPosition(chatAdapter.itemCount - 1)
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "Failed to send message:\n${it.message}")
+            }
+
+        refTo.setValue(chatMessage)
+            .addOnSuccessListener {
+                Log.d(TAG, "Saved our chat message: ${refTo.key}")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "Failed to send message:\n${it.message}")
+            }
     }
 }
