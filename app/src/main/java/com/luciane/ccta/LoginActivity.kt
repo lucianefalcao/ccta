f78b6ae2ada1a4edf58783f34bcf60c7d1527f12
João Paulo Silva Martins <joaopsilvam88gmail.com>
2021-11-26 09:55:11 -0300

    feat: Realizando autenticação do usuário com o Firebase

--- app/src/main/java/com/luciane/ccta/LoginActivity.kt
+++ app/src/main/java/com/luciane/ccta/LoginActivity.kt
@@ -3,8 +3,14 @@ package com.luciane.ccta
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,5 +30,60 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, ForgotPasswordActivity::class.java))
             finish()
         }
+
+        val login = findViewById<Button>(R.id.button)
+        val et_login_email = findViewById<TextInputEditText>(R.id.email)
+        val et_login_password = findViewById<TextInputEditText>(R.id.senha)
+
+        login.setOnClickListener {
+            when{
+                TextUtils.isEmpty(et_login_email.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Informa teu e-mail",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(et_login_password.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Informa tua senha",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val email: String = et_login_email.text.toString().trim{it <= ' '}
+                    val senha: String = et_login_password.text.toString().trim{it <= ' '}
+
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha)
+                        .addOnCompleteListener{ task ->
+
+                                if(task.isSuccessful){
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        "Você fez login com sucesso.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent = Intent(this@LoginActivity, AboutCenterActivity::class.java)
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                                    intent.putExtra("email_id", email)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                                else{
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/luciane/ccta/RegistrationActivity.kt
+++ app/src/main/java/com/luciane/ccta/RegistrationActivity.kt
@@ -3,7 +3,15 @@ package com.luciane.ccta
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +30,62 @@ class RegistrationActivity : AppCompatActivity() {
         privacityPolitics.setOnClickListener {
             startActivity(Intent(this, TermsActivity::class.java))
             finish()
+        }
+
+        val register = findViewById<Button>(R.id.button)
+        val et_register_email = findViewById<TextInputEditText>(R.id.email)
+        val et_register_password = findViewById<TextInputEditText>(R.id.senha)
+
+        register.setOnClickListener {
+            when{
+                TextUtils.isEmpty(et_register_email.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Informa teu e-mail",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(et_register_password.text.toString().trim{it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Informa tua senha",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val email: String = et_register_email.text.toString().trim{it <= ' '}
+                    val senha: String = et_register_password.text.toString().trim{it <= ' '}
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+
+                                if(task.isSuccessful){
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                                    Toast.makeText(
+                                        this@RegistrationActivity,
+                                        "Você se cadastrou com sucesso.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent = Intent(this@RegistrationActivity, AboutCenterActivity::class.java)
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra("user_id", firebaseUser.uid)
+                                    intent.putExtra("email_id", email)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                                else{
+                                    Toast.makeText(
+                                        this@RegistrationActivity,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        )
+                }
+            }
         }
     }
 }
