3ca95e934d424687ffb0fe0b7dfbfe14df07ba97
moura1001 <gmouraneto01@gmail.com>
2021-11-27 12:41:22 -0300

    commit com layout da tela Home

--- app/src/main/java/com/luciane/ccta/HomeActivity.kt
+++ app/src/main/java/com/luciane/ccta/HomeActivity.kt
@@ -1,36 +1,31 @@
 package com.luciane.ccta
 
-import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.ClipData.Item
 import android.content.Intent
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.ImageButton
 import com.luciane.ccta.activity.chat.ChatActivity
-import com.luciane.ccta.model.User
-
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val userInfo = getIntent();
-        val user = userInfo.getParcelableExtra<User>("User");
-        val login = user?.login;
-        val password = user?.password;
+        findViewById<ImageButton>(R.id.sobreButtonUserInputHome).setOnClickListener {
+            val intent = Intent(this, AboutCenterActivity::class.java)
+            startActivity(intent)
+        }
 
-        val viewLogin = findViewById<TextView>(R.id.viewLogin) as TextView
-        viewLogin.text = "Login: "+login
+        findViewById<ImageButton>(R.id.eventosButtonUserInputHome).setOnClickListener {
 
-        val viewPassword = findViewById<TextView>(R.id.viewPassword) as TextView
-        viewPassword.text = "Senha: "+password
+        }
 
-        val chatButton = findViewById<Button>(R.id.chatButton)
-        chatButton.setOnClickListener {
+        findViewById<ImageButton>(R.id.editaisButtonUserInputHome).setOnClickListener {
+
+        }
+
+        findViewById<ImageButton>(R.id.chatButtonUserInputHome).setOnClickListener {
             val intent = Intent(this, ChatActivity::class.java)
             startActivity(intent)
         }
