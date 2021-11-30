22b13e437f0017c7aae33ac421bd7201bf3ee4ed
moura1001 <gmouraneto01@gmail.com>
2021-11-30 11:34:58 -0300

    commit com listagem dos 3 editais mais recentes na tela de Home

--- app/src/main/java/com/luciane/ccta/HomeActivity.kt
+++ app/src/main/java/com/luciane/ccta/HomeActivity.kt
@@ -1,13 +1,28 @@
 package com.luciane.ccta
 
+import android.app.DownloadManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import com.luciane.ccta.activity.chat.ChatActivity
 import com.luciane.ccta.activity.editais.EditaisActivity
+import com.luciane.ccta.activity.editais.EditaisAdapter
+import com.luciane.ccta.model.Edital
 
 class HomeActivity : AppCompatActivity() {
+    companion object{
+        val TAG = "HomeScreen"
+    }
+
+    private var recyclerViewEditais: RecyclerView? = null
+    private var editaisAdapter = EditaisAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -31,5 +46,36 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL ,false)
+        editaisAdapter.downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        recyclerViewEditais = findViewById(R.id.recyclerViewEditaisHome)
+        recyclerViewEditais!!.setAdapter(editaisAdapter)
+        recyclerViewEditais!!.layoutManager = layoutManager
+        listenForEditais()
+
+    }
+
+    private fun listenForEditais(){
+        val ref = FirebaseFirestore.getInstance().collection("editais")
+            .orderBy("lastModified").limitToLast(3)
+        ref.addSnapshotListener { snapshot, error ->
+            if (error != null) {
+                Log.w(TAG, "Error on fetch data: ", error)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null) {
+                val editais = mutableListOf<Edital>()
+                for (doc in snapshot) {
+                    val id = doc.id
+                    val title = doc.getString("title")!!
+                    val documentPath = doc.getString("documentPath")!!
+                    val userUid = doc.getString("userUid")!!
+                    val lastModified = doc.getLong("lastModified")!!
+                    editais.add(Edital(id, title, documentPath, userUid, lastModified))
+                }
+                editaisAdapter.setEditaisList(editais)
+            }
+        }
     }
 }
