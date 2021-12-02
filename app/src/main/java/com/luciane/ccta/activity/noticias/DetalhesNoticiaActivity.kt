cd2f200a869e57160c772ba4babce414b18fd666
moura1001 <gmouraneto01@gmail.com>
2021-12-02 17:20:20 -0300

    commit com adição da tela de detalhes de uma notícia (sem integração com o Firebase)

---
+++ app/src/main/java/com/luciane/ccta/activity/noticias/DetalhesNoticiaActivity.kt
@@ -0,0 +1,12 @@
+package com.luciane.ccta.activity.noticias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.luciane.ccta.R
+
+class DetalhesNoticiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_noticia)
+    }
+}
