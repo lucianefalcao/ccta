c3ca8b8499b4bf493d92137f3c7e5d269748aeee
moura1001 <gmouraneto01@gmail.com>
2021-12-04 10:40:14 -0300

    commit com refatoração no adapter e model dos editais

--- app/src/main/java/com/luciane/ccta/model/Edital.kt
+++ app/src/main/java/com/luciane/ccta/model/Edital.kt
@@ -1,20 +1,4 @@
 package com.luciane.ccta.model
 
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-
 class Edital(var id: String, var title: String, var documentPath: String,
-             var userUid: String, var lastModified: Long) {
-
-    public fun getFormatedLastModifiedDateTime(): String{
-        val dateTime = LocalDateTime.ofInstant(
-            Instant.ofEpochMilli(this.lastModified),
-            ZoneId.systemDefault()
-        )
-        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG, FormatStyle.SHORT)
-        return dateTime.format(formatter)
-    }
-}
+             var userUid: String, var lastModified: Long)
